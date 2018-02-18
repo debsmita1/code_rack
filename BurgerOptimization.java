9b29e620df0138f5ac4939d1ed90a1f1e90f5f80
Debsmita <debsmita.santra@gmail.com>
2018-02-18 18:26:28 +0530

    BurgerOptimization.java

---
+++ BurgerOptimization.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+
+
+class BurgerOptimization {
+    public static void main(String args[] ) throws Exception {
+       
+       Scanner sc= new Scanner(System.in);
+    	
+       int t=sc.nextInt();
+       int i=1;
+       while(i<=t)
+       {
+           int k=sc.nextInt();
+           
+           int arr1[]= new int[k];
+           int arr2[]=new int[k];
+           int answer=0;
+           
+           for(int j=0;j<k;j++)
+           {
+              arr2[j]=sc.nextInt();  
+        	  
+           }
+           
+           Arrays.sort(arr2);
+           int index=0;
+           
+           if(k%2==0)
+           {
+               for(int l=0;l<k/2;l++)
+               {
+                   arr1[index]=arr1[index+1]=l;
+                   index=index+2;
+               }
+           }
+           else
+           {
+               for(int l=0;l<k/2;l++)
+               {
+                   arr1[index]=arr1[index+1]=l;
+                   index=index+2;
+               }
+               arr1[index]=k/2;
+           }
+           for(int l=0;l<k;l++)
+           {
+               answer+=Math.pow((arr1[l]-arr2[l]),2);
+           }
+           
+           System.out.println("Case #"+i+": "+answer);
+           i++;
+       }
+    }
+}
